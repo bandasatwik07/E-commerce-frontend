078f8fa794e88b71e4332934fd9574e781517ffe
Satwik <bandasatwik4@gmail.com>
2023-12-29 00:08:04 +0530

    part-2

--- src/features/navbar/Navbar.js
+++ src/features/navbar/Navbar.js
@@ -3,7 +3,8 @@ import { Fragment } from 'react'
 import { Disclosure, Menu, Transition } from '@headlessui/react'
 import { Bars3Icon, ShoppingCartIcon, XMarkIcon } from '@heroicons/react/24/outline'
 import { Link } from 'react-router-dom';
-
+import { useSelector } from 'react-redux';
+import { selectItems } from '../cart/cartSlice';
 const user = {
     name: 'Tom Cook',
     email: 'tom@example.com',
@@ -26,6 +27,7 @@ function classNames(...classes) {
 }
 
 const Navbar = ({ children }) => {
+    const items =useSelector(selectItems);
     return (
         <>
 
@@ -37,13 +39,11 @@ const Navbar = ({ children }) => {
                                 <div className="flex h-16 items-center justify-between">
                                     <div className="flex items-center">
                                         <div className="flex-shrink-0">
-                                            <Link to="/">
                                             <img
                                                 className="h-8 w-8"
                                                 src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=500"
                                                 alt="Your Company"
                                             />
-                                            </Link>
                                         </div>
                                         <div className="hidden md:block">
                                             <div className="ml-10 flex items-baseline space-x-4">
@@ -76,9 +76,9 @@ const Navbar = ({ children }) => {
                                                     <ShoppingCartIcon className="h-6 w-6" aria-hidden="true" />
                                                 </button>
                                             </Link>
-                                            <span className="relative inline-flex mb-7 -ml-3 items-center rounded-md  bg-red-50  px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                                                3
-                                            </span>
+                                            {items.length>0 && <span className="relative inline-flex mb-7 -ml-3 items-center rounded-md  bg-red-50  px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                                                {items.length}
+                                            </span>}
 
                                             {/* Profile dropdown */}
                                             <Menu as="div" className="relative ml-3">
@@ -176,9 +176,9 @@ const Navbar = ({ children }) => {
                                             </button>
                                         </Link>
 
-                                        <span className="inline-flex items-center rounded-md mb-3 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
-                                            3
-                                        </span>
+                                        {items.length>0 && <span className="inline-flex items-center rounded-md mb-3 -ml-3 bg-red-50 px-2 py-1 text-xs font-medium text-red-700 ring-1 ring-inset ring-red-600/10">
+                                            {items.length}
+                                        </span>}
                                     </div>
                                     <div className="mt-3 space-y-1 px-2">
                                         {userNavigation.map((item) => (
--- src/features/product-list/productAPI.js
+++ src/features/product-list/productAPI.js
@@ -1,30 +1,60 @@
 
-export function fetchAllProducts() {
+// export function fetchAllProducts() {
+//   return new Promise(async(resolve) =>{
+//     const response = await fetch('http://localhost:8080/products')
+//     const data = await response.json()
+//     resolve({data});
+//   }
+//   );
+// }
+
+export function fetchProductsById(id) {
   return new Promise(async(resolve) =>{
-    const response = await fetch('http://localhost:8080/products')
+    const response = await fetch('http://localhost:8080/products/'+id)
     const data = await response.json()
     resolve({data});
   }
   );
 }
 
-export function fetchProductsByFilters(filter) { 
-  //filter is an object
-
-  //filter = {
-  //  "category": "electronics",
-  //  "brand": "apple",
-  //  "price": "100-200",
-  //  "sort": "price-asc"
-  //}
-
+export function fetchProductsByFilters(filter,sort,pagination) { 
+  //filter ={category: ['electronics', 'jewelery'], price: [100, 200]}
+  //sort={_sort: 'price', _order: 'asc'}
+  //paination={_page: 1, _limit: 10}
   let queryString = '';
   for (let key in filter) {
-    queryString += `${key}=${filter[key]}&`;
-    console.log(`${key}=${filter[key]}&`);
+    for (let i = 0; i < filter[key].length; i++) {
+      queryString += `${key}=${filter[key][i]}&`;
+    }
   }
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
+  }
+  for (let key in pagination) {
+    queryString += `${key}=${pagination[key]}&`;
+  }
+  console.log(queryString);
+
   return new Promise(async(resolve) =>{
     const response = await fetch('http://localhost:8080/products?'+queryString)
+    const data = await response.json()
+    const totalItems = response.headers.get('X-Total-Count');
+    resolve({data:{products:data,totalItems:+totalItems}});
+  }
+  );
+}
+
+export function fetchCategories() {
+  return new Promise(async(resolve) =>{
+    const response = await fetch('http://localhost:8080/categories')
+    const data = await response.json()
+    resolve({data});
+  }
+  );
+}
+export function fetchBrands() {
+  return new Promise(async(resolve) =>{
+    const response = await fetch('http://localhost:8080/brands')
     const data = await response.json()
     resolve({data});
   }
