0c492b8ab664218c9cc8457e60bd95d425891c9a
unknown <bandasatwik4@gmail.com>
2025-02-08 16:23:45 +0530

    dependecy update

--- src/pages/AdminProductDetailPage.js
+++ src/pages/AdminProductDetailPage.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import NavBar from '../features/navbar/Navbar'
 import AdminProductDetail from '../features/admin/components/AdminProductDetail'
+
 export const AdminProductDetailPage = () => {
   return (
     <div>
